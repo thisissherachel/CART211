1ad40d5cc38bb8fc8813a1fb677967b4ebe08963
mathi330 <mathilded@protonmail.com>
2021-11-29 14:04:53 -0500

    Changed colors and added movement

--- ExplorePostInternet/z-HTML/js/myscript.js
+++ ExplorePostInternet/z-HTML/js/myscript.js
@@ -14,6 +14,7 @@ let sketch = function (p) {
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.background(255);
+
     p.rectMode(p.CENTER);
 
     for (let i = 0; i < numSquares; i++) {
@@ -23,20 +24,29 @@ let sketch = function (p) {
   };
 
   p.createSquares = function () {
-    let mySize = p.random(500, 700);
+    let mySize = p.random((p.height / 5) * 3, (p.width / 5) * 3);
+    let alphaValue = 50;
+    let fillOptions = [
+      p.color(255, 99, 214, alphaValue),
+      p.color(0, 12, 255, alphaValue),
+      p.color(255, 247, 0, alphaValue),
+      p.color(88, 255, 99, alphaValue),
+      p.color(255, 72, 30, alphaValue),
+    ];
+    let mySpeed = p.random(-0.8, 0.8);
     let square = {
       x: p.random(0, p.width),
       y: p.random(0, p.height),
       size: mySize,
-      fill: {
-        r: p.random(100, 255),
-        g: p.random(80, 180),
-        b: p.random(100, 255),
-        a: 30,
-      },
+      fill: p.random(fillOptions),
+
+      turnRate: 0.003,
+      speed: 0.8,
+      vx: mySpeed,
+      vy: mySpeed,
 
       angle: p.random(-100, 100),
-      rotate: p.random(-0.001, 0.001),
+      rotate: p.random(-0.003, 0.003),
     };
     return square;
   };
@@ -47,11 +57,29 @@ Description of draw()
   p.draw = function () {
     p.background(255);
 
+    p.displaySquares();
+  };
+
+  p.displaySquares = function () {
     for (let i = 0; i < squares.length; i++) {
       let square = squares[i];
       p.push();
+
+      let r = p.random(0, 1);
+      if (r < square.turnRate) {
+        square.vx = p.random(-square.speed, square.speed);
+        square.vy = p.random(-square.speed, square.speed);
+        square.rotate = -square.rotate;
+      }
+
+      square.x += square.vx;
+      square.y += square.vy;
+
+      square.x = p.constrain(square.x, -10, p.width + 10);
+      square.y = p.constrain(square.y, -10, p.height + 10);
+
       p.noStroke();
-      p.fill(square.fill.r, square.fill.g, square.fill.b, square.fill.a);
+      p.fill(square.fill);
 
       square.angle += square.rotate;
 
@@ -61,6 +89,10 @@ Description of draw()
       p.rect(0, 0, square.size);
       p.pop();
     }
+  };
+
+  p.windowResized = function () {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
   };
 };
 
