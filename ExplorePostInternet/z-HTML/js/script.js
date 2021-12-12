1679ea5ce134231b81524df4aca051e0656b47c6
mathi330 <mathilded@protonmail.com>
2021-12-12 13:29:44 -0500

    hover js (not perfectly working)

--- ExplorePostInternet/z-HTML/js/script.js
+++ ExplorePostInternet/z-HTML/js/script.js
@@ -1,14 +1,23 @@
+// create an array to store the elements of the quote-closed class
 let quotesArray = document.getElementsByClassName("img-quotes");
+// For loop to call every element
 for (let i = 0; i < quotesArray.length; i++) {
-  let img1 = quotesArray[i];
-  img1.addEventListener("mouseover", grow);
-  img1.addEventListener("mouseleave", normal);
-}
+  let block = quotesArray[i];
+  block.addEventListener(
+    "mouseover",
+    (e) => {
+      let block = e.target;
+      block.style.transform = "scale(1)";
+    },
+    false
+  );
 
-function grow(e) {
-  e.target.style.transform = "scale(1)";
-}
-
-function normal(e) {
-  e.target.style.transform = "scale(0.65)";
+  block.addEventListener(
+    "mouseleave",
+    (e) => {
+      let block = e.target;
+      block.style.transform = "scale(0.65)";
+    },
+    true
+  );
 }
