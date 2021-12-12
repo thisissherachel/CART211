0ff95d92c836f2926fdb9d84e4cdc38654faafcd
mathi330 <mathilded@protonmail.com>
2021-12-12 17:15:48 -0500

    hover text and image big

--- ExplorePostInternet/HTML/js/script.js
+++ ExplorePostInternet/HTML/js/script.js
@@ -1,5 +1,5 @@
 // create an array to store the elements of the quote-closed class
-let quotesArray = document.getElementsByClassName("img-quotes");
+let quotesArray = document.getElementsByClassName("shapes");
 // For loop to call every element
 for (let i = 0; i < quotesArray.length; i++) {
   let block = quotesArray[i];
@@ -8,8 +8,21 @@ for (let i = 0; i < quotesArray.length; i++) {
     (e) => {
       let block = e.target;
       block.style.transform = "scale(1)";
+      let masterBlock = block.parentNode.parentNode;
+      let oneChild = masterBlock.firstElementChild;
+      while (
+        !(
+          oneChild.matches(".txt-quotes") ||
+          oneChild === masterBlock.lastElementChild
+        )
+      ) {
+        oneChild = oneChild.nextElementSibling;
+      }
+      if (oneChild.matches(".txt-quotes")) {
+        oneChild.style.transform = "scale(1)";
+      }
     },
-    false
+    true
   );
 
   block.addEventListener(
@@ -17,6 +30,19 @@ for (let i = 0; i < quotesArray.length; i++) {
     (e) => {
       let block = e.target;
       block.style.transform = "scale(0.65)";
+      let masterBlock = block.parentNode.parentNode;
+      let oneChild = masterBlock.firstElementChild;
+      while (
+        !(
+          oneChild.matches(".txt-quotes") ||
+          oneChild === masterBlock.lastElementChild
+        )
+      ) {
+        oneChild = oneChild.nextElementSibling;
+      }
+      if (oneChild.matches(".txt-quotes")) {
+        oneChild.style.transform = "scale(0.65)";
+      }
     },
     true
   );
